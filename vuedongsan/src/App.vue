b9ec0be0e9c22b6b19c0d28520ed71ee9bf530cd
kohj1018 <kohj1018@gmail.com>
2021-09-03 23:55:40 +0900

    first commit

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+  </div>
+
+  
+  <div v-for="element in products" :key="element">
+    <h4>{{ element[0] }}</h4>
+    <p>{{ element[1] }} 만원</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      menus : ['Home', 'Shop', 'About'],
+      products : [['역삼동원룸', 60], ['천호동원룸', 70], ['마포구원룸', 65]]
+    }
+  },
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
